60d959b4855171491f61cdc175a5d55b2c49deeb
Dario Castagnola <dcastagn@e4r6p2.42roma.it>
2023-11-02 11:36:27 +0100

    ex00 added test

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dcastagn <dcastagn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:02:28 by dcastagn          #+#    #+#             */
-/*   Updated: 2023/10/27 16:23:37 by dcastagn         ###   ########.fr       */
+/*   Updated: 2023/11/02 11:35:09 by dcastagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ int main( void )
 {
 	int a = 2;
 	int b = 3;
+	std::cout << "before swap : a = " << a << ", b = " << b << std::endl;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "after swap : a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	std::cout << "before swap : c = " << c << ", d = " << d << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "after swap : c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
